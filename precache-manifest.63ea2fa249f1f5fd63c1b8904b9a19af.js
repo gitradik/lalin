c8842fd2343775e89c6b4377770642de7741b371
Родион Малахов <rminzp17@gmail.com>
2019-04-25 20:01:07 +0300

    Updates

---
+++ precache-manifest.63ea2fa249f1f5fd63c1b8904b9a19af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820d547f75a58406e615ca6a71d1a9ac",
+    "url": "/lalin/index.html"
+  },
+  {
+    "revision": "681eac441a8027d1fa7f",
+    "url": "/lalin/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/lalin/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "681eac441a8027d1fa7f",
+    "url": "/lalin/static/js/main.5791b14b.chunk.js"
+  },
+  {
+    "revision": "4414672a97511678aeb9",
+    "url": "/lalin/static/js/runtime~main.909a04ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lalin/static/media/logo.5d5d9eef.svg"
+  }
+]);
