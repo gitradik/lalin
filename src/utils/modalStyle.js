a46f169e847f8ccf38135746fbc0352d20a71b7c
Родион Малахов <rminzp17@gmail.com>
2019-04-27 14:12:50 +0300

    media

--- src/utils/modalStyle.js
+++ src/utils/modalStyle.js
@@ -2,10 +2,12 @@ export const modalStyle = {
     closeButton: {
         fill: "#ff28ed",
         right: '0',
-        marginTop: '-50px'
+        marginTop: '-50px',
+        marginRight: '10px'
     },
     overlay: {
         padding: '0',
+        paddingTop: '50px',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
